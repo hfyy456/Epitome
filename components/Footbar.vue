3bb29e4b1318e234f1bff68342c24c042407c878
hfyy456 <540363975@qq.com>
2020-06-12 10:07:16 +0800

    aa

---
+++ components/Footbar.vue
@@ -0,0 +1,84 @@
+<template>
+  <div class="content">
+    <ul class="foot-ul">
+      <li>
+        <a class="link">About</a>
+      </li>
+      <li>
+        <a class="link">Contact</a>
+      </li>
+      <li>
+        <a class="link">Licensing</a>
+      </li>
+      <li>
+        <a class="link">Download</a>
+      </li>
+
+      <li>
+        <div>
+          <ul class="ul-social">
+            <li>
+              <a-icon type="github" class="icons" />
+            </li>
+            <li>
+              <a-icon type="qq" class="icons" />
+            </li>
+            <li>
+              <a-icon type="wechat" class="icons" />
+            </li>
+          </ul>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+<style lang="less" scoped>
+.content {
+  padding: 16px;
+  height: 56px;
+  background-color: black;
+  .link {
+    color: hsla(0, 0%, 100%, 0.6);
+    &:hover {
+      cursor: pointer;
+      color: #fff;
+    }
+    > li {
+      display: inline-block;
+    }
+  }
+  .foot-ul {
+    color: white;
+    list-style: none;
+    margin: 0;
+    display: flex;
+    justify-content: space-around;
+  }
+  .ul-social {
+    display: flex;
+    list-style: none;
+    font-size: 20px;
+    color: hsla(0, 0%, 100%, 0.6);
+    justify-content: center;
+    .icons {
+      &:hover {
+        cursor: pointer;
+        color: #fff;
+      }
+      margin-left: 20px;
+      position: relative;
+      bottom: 4px;
+    }
+  }
+  ul {
+    padding: 0%;
+  }
+}
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,46 @@
+<template>
+  <Layout>
+    <Header><Navbar/></Header>
+    <Layout>
+      <Content>
+        <nuxt />
+      </Content>
+    </Layout>
+    <Footer><Footbar /></Footer>
+  </Layout>
+</template>
+<script>
+import Navbar from '~/components/Navbar.vue'
+import Footbar from '~/components/Footbar.vue'
+
+export default {
+  components: {
+    Navbar,
+    Footbar
+  },
+  data() {
+    return {}
+  }
+
+}
+</script>
+<style>
+html {
+  font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI",
+    Roboto, "Helvetica Neue", Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+  margin: 0;
+}
+</style>
