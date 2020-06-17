ce06176b1edfaac538769b94367e6ea7520dd45f
hfyy456 <540363975@qq.com>
2020-06-17 22:14:18 +0800

    更新相册的作者卡

--- pages/photo/_id.vue
+++ pages/photo/_id.vue
@@ -1,26 +1,62 @@
 <template>
   <div>
-    <div class="photo-container">
+    <div class="photo-container" :style="style">
       <div class="wrapper">
         <nuxt-link to="/gallery/popular">
           <a-icon type="arrow-left" class="left" />
         </nuxt-link>
         <img :src="baseUrl" class="photo" />
-        <a-icon type="arrows-alt" class="right" />
+        <a-icon v-if="!full" type="arrows-alt" class="right" @click="fullscreen" />
+        <a-icon v-else type="shrink" class="right" @click="exitScreen" />
       </div>
     </div>
+    <div class="information-box">
+      <div class="author">
+        <div class="actions">
+          <a-button type="primary" shape="square" icon="download" :size="size" />
+          <a-button type="primary" shape="square" icon="star" :size="size" />
+          <a-button type="primary" shape="square" icon="share-alt" :size="size" />
+        </div>
+        <authorCard />
+        <a-divider />
+      </div>
+    </div>
+    <div class="comment-box"></div>
   </div>
 </template>
 <script>
+import authorCard from '~/components/authorCard.vue'
 const baseUrl =
   'https://drscdn.500px.org/photo/1017302892/q%3D80_m%3D2000/v2?sig=839793636a7cc69021af290c4f90a0f5a38f0fed10bf7a316ff481d9687ff8e3'
 export default {
+  components: {
+    authorCard
+  },
   data() {
     return {
-      baseUrl
+      baseUrl,
+      style: {},
+      full: false
     }
   },
-  methods: {}
+  methods: {
+    fullscreen() {
+      this.style = {
+        position: 'absolute',
+        top: 0,
+        'z-index': 999,
+        width: '100vw',
+        height: '100vh'
+      }
+      this.full = true
+      document.documentElement.requestFullscreen()
+    },
+    exitScreen() {
+      this.style = {}
+      this.full = false
+      document.exitFullscreen()
+    }
+  }
 }
 </script>
 <style lang="less" scoped>
@@ -68,5 +104,18 @@ export default {
       object-fit: contain;
     }
   }
+}
+.information-box {
+  background-color: #f5f5f5;
+  padding: 10px;
+  display: inline-block;
+  .author {
+    padding: 10px;
+    background-color: #fff;
+  }
+}
+.comment-box {
+    display: inline-block;
+
 }
 </style>
